5a3b81e5861bda959d55ef91c7fcd07d7322fedd
LalkrishnaGadamsetty <lalkrishna0703@gmail.com>
2023-10-16 14:34:52 +0530

    initial commit

---
+++ src/components/Contact.js
@@ -0,0 +1,31 @@
+// Contact.js
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>I'm always open to new opportunities and collaborations.</p>
+      <ul>
+        <li>
+          <strong>Email:</strong> <a href=" ">lalkrishna.gadamsetty@gmail.com</a>
+        </li>
+        <li>
+          <strong>Instagram</strong>{" "}
+          <a href=" #" target="_blank" rel="noopener noreferrer">
+          
+          </a>
+        </li>
+        <li>
+          <strong>GitHub:</strong>{" "}
+          <a href=" https://github.com/LalkrishnaGadamsetty" target="_blank" rel="noopener noreferrer">
+            
+          </a>
+        </li>
+      </ul>
+      <p>I'm looking forward to connecting with you!</p>
+    </section>
+  );
+};
+
+export default Contact;
